d698e8e1441713cbbc548fc910a21c33b7eff7ea
meghanashalivaha <kummarim437@gmail.com>
2026-04-17 21:17:01 +0530

    Create mixheap.py

---
+++ mixheap.py
@@ -0,0 +1,29 @@
+def heapifyDown(nums,index):
+    smallest=index
+    leftchild=2*index+1
+    rightchild=2*index+2
+    if(leftchild<len(nums) and nums[leftchild]< nums[smallest]):
+        smallest=leftchild 
+    if(rightchild<len(nums) and nums[rightchild]< nums[smallest]):
+       smallest=rightchild
+    if(smallest!=index):
+        nums[index],nums[smallest]=nums[smallest],nums[index]
+        heapifyDown(nums,smallest)
+def heapifyUp(nums,index):
+    if(index>0):
+        parent=(index-1)//2
+        if(nums[index]<nums[parent]):
+            nums[index],nums[parent]=nums[parent],nums[index]
+            heapifyUp(nums,parent)
+nums=[1,4,5,6,7,10,6,3,2]
+n=int(input())
+for _ in range(0,n):
+    index=int(input())
+    value=int(input())
+    if(nums[index]>value):
+        nums[index]=value
+        heapifyDown(nums,index)
+    if(nums[index]<value):
+        nums[index]=value
+        heapifyUp(nums,index)
+print(nums)
